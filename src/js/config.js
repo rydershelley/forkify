4a958bcdbbd7bccb6b0dc978969fd77c6e65ca1b
rydershelley <rydershelley@hotmail.com>
2023-12-15 15:28:06 -0500

    Intitial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'b5c2e5ba-77af-485c-917f-7935f9991cde';
+export const MODAL_CLOSE_SEC = 2.5;
